d50499443110b69816356ca6269936ef7120efa1
lanbinshijie <huoziye123@163.com>
2023-11-03 11:41:30 +0800

    feat: 改善了代码逻辑，新增了静态映射

--- index.py
+++ index.py
@@ -1,18 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from core import *
 
-app = Flask(__name__)
-
-from markdown_it import MarkdownIt
-from mdit_py_plugins.front_matter import front_matter_plugin
-from mdit_py_plugins.footnote import footnote_plugin
-
-md = (
-    MarkdownIt('commonmark' ,{'breaks':True,'html':True})
-    .use(front_matter_plugin)
-    .use(footnote_plugin)
-    .enable('table')
-)
+app = Flask(__name__, static_folder="assets")
 
 def generate_html_list(data):
     html = "<ul>\n"
@@ -28,15 +17,15 @@ def generate_html_list(data):
     return html
 
 introDuction = """
-<p>期中考试临近，编写了一些复习资料，但苦于不喜欢收纳整理文件，所以想将它们都防盗网站是，故做了这个CMS系统，目前开发中，仅支持阅读。如果你觉得这个项目还可以，欢迎去Github点一个Star～</p>
+<p>期中考试临近，编写了一些复习资料，但苦于不喜欢收纳整理文件，所以想将它们都放到网站上，故做了这个CMS系统，目前开发中，仅支持阅读。如果你觉得这个项目还可以，欢迎去Github点一个Star～</p>
 """
 
 @app.route("/")
 def index():
     # 调用 getNewestMD 函数获取 Markdown 内容
-    print(list_and_cache_passages())
+    # print(list_and_cache_passages())
     markdown_list = generate_html_list(list_and_cache_passages())
-    print(markdown_list)
+    # print(markdown_list)
     
     # 渲染模板并传递 HTML 内容
     return render_template("index.html", content=introDuction+markdown_list, namer="主页")
@@ -51,11 +40,14 @@ def save():
 def readPassage(idx):
     # 调用 getNewestMD 函数获取 Markdown 内容
     name, markdown_text = getById(idx)
-    html_text = md.render(markdown_text)
-    
+    html_text = render_html(markdown_text, idx)
 
     # 渲染模板并传递 HTML 内容
-    return render_template("index.html", content=html_text, namer=name)
+    return render_template("p.html", content=html_text, namer=name)
+
+@app.route('/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
